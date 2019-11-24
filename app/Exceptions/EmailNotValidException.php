641a295804276a791da8ac52285889f866470db5
Alex Jenkinson <alex.jenkinson@verdite.co.uk>
2019-11-24 22:43:14 +0000

    Added custom exception error message

--- app/Exceptions/EmailNotValidException.php
+++ app/Exceptions/EmailNotValidException.php
@@ -4,4 +4,8 @@ namespace App\Exceptions;
 
 class EmailNotValidException extends \Exception
 {
+    public function errorMessage()
+    {
+        return '<b>'.$this->getMessage().'</b> is not a valid email address.';
+    }
 }
