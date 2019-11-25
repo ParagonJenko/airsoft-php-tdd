13ad216b777a66ba816529af117596517d33b02f
Alex Jenkinson <alex.jenkinson@verdite.co.uk>
2019-11-25 13:57:52 +0000

    Added if type, if not type and either exception or bool returns

--- app/Support/Validation.php
+++ app/Support/Validation.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Support;
 
+use App\Exceptions\NotEnougCharacterException;
+
 class Validation
 {
     public static function removeAllWhiteSpace($string)
@@ -11,10 +13,84 @@ class Validation
     public static function checkStringLength($string, $operator, $lengthInteger)
     {
         switch ($operator) {
-            case "==": return strlen($string) == $lengthInteger;
-            case ">=": return strlen($string) >= $lengthInteger;
-            case "<=": return strlen($string) <= $lengthInteger;
-            case "!=": return strlen($string) != $lengthInteger;
+            case "==":
+                if (!strlen($string) == $lengthInteger) {
+                    throw new NotEnougCharacterException;
+                }
+                break;
+            case ">=":
+                if (!strlen($string) >= $lengthInteger) {
+                    throw new NotEnougCharacterException;
+                }
+                break;
+            case "<=":
+                if (!strlen($string) <= $lengthInteger) {
+                    throw new NotEnougCharacterException;
+                }
+                break;
+            case "!=":
+                if (!strlen($string) != $lengthInteger) {
+                    throw new NotEnougCharacterException;
+                }
+                break;
         };
+
+        return true;
+    }
+
+    public static function checkIfNotType($variable, $type, $returnException = true)
+    {
+        switch ($type) {
+            case "string":
+                if (!is_string($variable)) {
+                    if ($returnException) {
+                        throw new TypeNotValidException;
+                    } else {
+                        return false;
+                    }
+                }
+                break;
+            case "integer":
+                if (!is_numeric($variable)) {
+                    if ($returnException) {
+                        throw new TypeNotValidException;
+                    } else {
+                        return false;
+                    }
+                }
+                break;
+            case "email_address":
+                if (!filter_var($variable, FILTER_VALIDATE_EMAIL)) {
+                    if ($returnException) {
+                        throw new TypeNotValidException;
+                    } else {
+                        return false;
+                    }
+                }
+                break;
+        }
+        return true;
+    }
+
+    public static function checkIfIsType($variable, $type)
+    {
+        switch ($type) {
+            case "string":
+                if (is_string($variable)) {
+                    return true;
+                }
+                break;
+            case "integer":
+                if (is_numeric($variable)) {
+                    return true;
+                }
+                break;
+            case "email_address":
+                if (filter_var($variable, FILTER_VALIDATE_EMAIL)) {
+                    return true;
+                }
+                break;
+        }
+        throw new TypeNotValidException;
     }
 }
